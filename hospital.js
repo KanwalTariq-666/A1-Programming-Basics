de17c41e8839036d31898342501271f1d5da71fa
KanwalTariq-666 <tariqka@sheridancollege.ca>
2024-09-25 22:56:44 -0400

    Part 1: Data Structure
    Create a data structure that describes a "hospital". It has a name and contains 3 "patient" records. Each "patient" has an id, fullName, dateOfBirth, and multiple "symptoms". Each symptom in a String.

---
+++ hospital.js
@@ -0,0 +1,32 @@
+//hospital.js
+
+// Part 1: 
+
+
+let hospital = {
+    name: "Toronto General Hospital", 
+    patients: [
+        {
+            ID: "ID#: ABC123", 
+            fullName: "Jane Doe",
+            dateOfBirth: "01-01-2001", 
+            symptoms: ["cough", "flu"]
+        },
+        {
+            ID: "ID#: JKL456", 
+            fullName: "John Smith",
+            dateOfBirth: "02-02-2002", 
+            symptoms: ["migraine", "nausea"]
+        },
+        {
+            ID: "ID#: XYZ789", 
+            fullName: "Jonathan Doey", 
+            dateOfBirth: "03-03-2003",
+            symptoms: ["dizziness", "low blood pressure"]
+        },
+    ]
+
+};
+
+
+
